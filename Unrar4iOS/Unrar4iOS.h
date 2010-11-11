66c0a0e78adbeb7007a6a66e4575f7ea97f4de0b
Rogério Araújo <rogerio.araujo@gmail.com>
2010-11-10 23:22:13 -0200

    Generating a framework package for this library.

---
+++ Unrar4iOS/Unrar4iOS.h
@@ -0,0 +1,18 @@
+//
+//  Unrar4iOS.h
+//  Unrar4iOS
+//
+//  Created by Rogerio Pereira Araujo on 10/11/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "raros.hpp"
+#import "dll.hpp"
+
+@interface Unrar4iOS : NSObject {
+
+	HANDLE rarFile;
+}
+
+@end
---
+++ Unrar4iOS/Unrar4iOS.mm
@@ -0,0 +1,13 @@
+//
+//  Unrar4iOS.mm
+//  Unrar4iOS
+//
+//  Created by Rogerio Pereira Araujo on 10/11/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "Unrar4iOS.h"
+
+@implementation Unrar4iOS
+
+@end
