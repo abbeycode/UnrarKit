e841c48e023f2c01e8ebd854e73f0bff334d2bcc
Dov Frankel <dov@abbey-code.com>
2014-12-18 10:26:17 -0500

    Fixed analyzer warning

--- Example/Classes/UnrarExampleViewController.m
+++ Example/Classes/UnrarExampleViewController.m
@@ -82,11 +82,6 @@
 	// Release any cached data, images, etc that aren't in use.
 }
 
-- (void)viewDidUnload {
-	// Release any retained subviews of the main view.
-	// e.g. self.myOutlet = nil;
-}
-
 
 - (void)dealloc {
     [super dealloc];
