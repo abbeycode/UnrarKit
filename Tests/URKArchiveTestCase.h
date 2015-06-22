5685f8c16a953b225358c15c9ba85ad87258877e
Dov Frankel <dov@abbey-code.com>
2015-06-22 10:25:24 -0400

    Refactored unit test setup/teardown and helper methods into a separate testing base class

---
+++ Tests/URKArchiveTestCase.h
@@ -0,0 +1,29 @@
+//
+//  URKArchiveTestCase.h
+//  UnrarKit
+//
+//  Created by Dov Frankel on 6/22/15.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface URKArchiveTestCase : XCTestCase
+
+@property BOOL testFailed;
+
+@property NSURL *tempDirectory;
+@property NSMutableDictionary *testFileURLs;
+@property NSMutableDictionary *unicodeFileURLs;
+@property NSURL *corruptArchive;
+
+
+- (NSURL *)urlOfTestFile:(NSString *)filename;
+- (NSString *)randomDirectoryName;
+- (NSString *)randomDirectoryWithPrefix:(NSString *)prefix;
+- (NSInteger)numberOfOpenFileHandles;
+- (NSURL *)randomTextFileOfLength:(NSUInteger)numberOfCharacters;
+- (NSURL *)archiveWithFiles:(NSArray *)fileURLs;
+- (NSUInteger)crcOfTestFile:(NSString *)filename;
+
+@end
