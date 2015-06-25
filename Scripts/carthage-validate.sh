2a13d51f2be05cca46238946c3b1c849c5f453be
Dov Frankel <dov@abbey-code.com>
2015-06-25 19:04:34 -0400

    Added carthage validation, which won't work 100% until my pull request (#583) is accepted on the Carthage project

---
+++ Scripts/carthage-validate.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ -z ${TRAVIS+x} ]; then
+    TRAVIS_BUILD_DIR="file:///Users/Dov/Source Code/UnrarKit"
+    TRAVIS_BRANCH=carthage
+fi
+
+if [ ! -d "CarthageValidation" ]; then
+    mkdir "CarthageValidation"
+fi
+
+pushd CarthageValidation > /dev/null
+
+rm Cartfile
+rm Cartfile.resolved
+rm -rf Carthage
+
+echo "git \"$TRAVIS_BUILD_DIR\" \"$TRAVIS_BRANCH\"" > Cartfile
+
+carthage bootstrap --configuration Debug --verbose --simulator-only
+EXIT_CODE=$?
+
+echo "Checking for build products..."
+
+if [ ! -d "Carthage/Build/Mac/UnrarKit.framework" ]; then
+    echo "No Mac library built"
+    EXIT_CODE=1
+fi
+
+if [ ! -d "Carthage/Build/iOS/UnrarKit.framework" ]; then
+    echo "No iOS library built"
+    EXIT_CODE=1
+fi
+
+popd > /dev/null
+
+exit $EXIT_CODE
