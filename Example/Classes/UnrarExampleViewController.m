ceec64d9ec411c2b33aadfaeeea356b143b917e7
Dov Frankel <dov@abbey-code.com>
2015-08-04 16:52:03 -0400

    Updated example project to complement the current UnrarKit syntax

--- Example/Classes/UnrarExampleViewController.m
+++ Example/Classes/UnrarExampleViewController.m
@@ -9,44 +9,22 @@
 
 @implementation UnrarExampleViewController
 
-/*
-// The designated initializer. Override to perform setup that is required before the view is loaded.
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-*/
-
-/*
-// Implement loadView to create a view hierarchy programmatically, without using a nib.
-- (void)loadView {
-}
-*/
-
-
-/*
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
-- (void)viewDidLoad {
-    [super viewDidLoad];
-}
-*/
-
-
-/*
-// Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
-*/
-
 - (IBAction)decompress:(id)sender {
     NSString *filePath = [[NSBundle mainBundle] pathForResource:@"Test Archive (Password)" ofType:@"rar"];
 
-	URKArchive *archive = [URKArchive rarArchiveAtPath:filePath];
+    NSError *archiveError = nil;
+	URKArchive *archive = [[URKArchive alloc] initWithPath:filePath error:&archiveError];
+    
+    if (!archive) {
+        UIAlertController *controller = [UIAlertController alertControllerWithTitle:@"Failed to create archive"
+                                                                            message:@"Error creating the archive"
+                                                                     preferredStyle:UIAlertControllerStyleAlert];
+        [controller addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
+        
+        [self presentViewController:controller animated:YES completion:nil];
+        return;
+    }
+    
     NSError *error = nil;
     NSArray *filenames = [archive listFilenames:&error];
     
@@ -73,14 +51,6 @@
         imageView.image = image;
     }
 }
-
-- (void)didReceiveMemoryWarning {
-	// Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-	
-	// Release any cached data, images, etc that aren't in use.
-}
-
 
 - (void)dealloc {
     [super dealloc];
