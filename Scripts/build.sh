63f21c98f3df1ad002b3cd1558e4c7971a231139
Dov Frankel <dov@abbey-code.com>
2016-12-14 20:35:41 -0500

    Switched from xctool to xcodebuild

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,1 +1,1 @@
-xctool -workspace $TRAVIS_XCODE_WORKSPACE -scheme "$TRAVIS_XCODE_SCHEME" -sdk $TRAVIS_XCODE_SDK -configuration Release analyze test
+set -o pipefail && xcodebuild -workspace $TRAVIS_XCODE_WORKSPACE -scheme "$TRAVIS_XCODE_SCHEME" -sdk $TRAVIS_XCODE_SDK -configuration Release analyze test | xcpretty
