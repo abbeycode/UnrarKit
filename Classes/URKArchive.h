b8b0baaa90164dff1c3dcf705695f4eb3e149eb4
Dov Frankel <dov@abbey-code.com>
2017-09-22 21:59:41 -0400

    Whoops. Committed header changes for progress property

--- Classes/URKArchive.h
+++ Classes/URKArchive.h
@@ -164,16 +164,20 @@ extern NSString *URKErrorDomain;
 @property(readonly) BOOL hasMultipleVolumes;
 =======
  *  Can be used for progress reporting, but it's not necessary. You can also use
- *  implicit progress reporting. Will be nil until an operation begins, and after one
- *  completes. Behavior is undefined when multiple operations are in progress
- *  concurrently. In that case, you should use implicit progress reporting and make
- *  your parent progress the current progress on each thread
+ *  implicit progress reporting. If you don't use it, one will still be created,
+ *  which will become a child progress of whichever one is the current NSProgress
+ *  instance.
  *
- *  The instance isn't assigned until the method has started, so you'll need to call
- *  it from a background thread in order to make use of it
+ *  To use this, assign it before beginning an operation that reports progress. Once
+ *  the method you're calling has a reference to it, it will nil it out. Please check
+ *  for nil before assigning it to avoid concurrency conflicts.
  */
+<<<<<<< HEAD
 @property(nullable, weak, readonly) NSProgress *progress;
 >>>>>>> Implemented NSProgressReporting protocol to make conformance more apparent
+=======
+@property(nullable, strong) NSProgress *progress;
+>>>>>>> Whoops. Committed header changes for progress property
 
 
 /**
