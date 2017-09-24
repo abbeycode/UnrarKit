660b9b1c7d8e4ae4cf7ed5d5cbfd8b30842a7a38
Dov Frankel <dov@abbey-code.com>
2017-09-24 13:17:03 -0400

    Updated documentation

--- Classes/URKArchive.h
+++ Classes/URKArchive.h
@@ -115,7 +115,14 @@ static URKProgressInfoKey _Nonnull
 =======
 typedef NSString *const URKProgressInfoKey;
 
+
+/**
+ *  Defines the keys passed in `-[NSProgress userInfo]` for certain methods
+ */
 static URKProgressInfoKey _Nonnull
+    /**
+     *  For `extractFilesTo:overwrite:error:`, this key contains an instance of URKFileInfo with the file currently being extracted
+     */
     URKProgressInfoKeyFileInfoExtracting = @"URKProgressInfoKeyFileInfoExtracting";
 >>>>>>> Added reporting of URKFileInfo objects to progress reporting for extractFiles
 >>>>>>> Added reporting of URKFileInfo objects to progress reporting for extractFiles
@@ -344,8 +351,8 @@ extern NSString *URKErrorDomain;
  *  Lists the paths of volumes in a single- or multi-volume archive
 =======
  *  Writes all files in the archive to the given path. Supports NSProgress for progress reporting, which also
- *  allows cancellation in the middle of extraction. Use the 'progress' property (as explained in the README)
- *  to retrieve more detailed information, such as the current file being extracted, number of files extracted,
+ *  allows cancellation in the middle of extraction. Use the progress property (as explained in the README) to
+ *  retrieve more detailed information, such as the current file being extracted, number of files extracted,
  *  and the URKFileInfo instance being extracted
  *
  *  @param filePath  The destination path of the unarchived files
@@ -359,6 +366,7 @@ extern NSString *URKErrorDomain;
                  error:(NSError **)error;
 
 /**
+<<<<<<< HEAD
  *  **DEPRECATED:** Writes all files in the archive to the given path. Supports NSProgress for progress reporting, which also
  *  allows cancellation in the middle of extraction
 >>>>>>> Deprecated methods that take a progress block argument, and created versions without them
@@ -389,7 +397,13 @@ extern NSString *URKErrorDomain;
 =======
 >>>>>>> Was at master, merging branch…
  *  Writes all files in the archive to the given path
+<<<<<<< HEAD
 >>>>>>> Rebased to multivolume branch
+=======
+=======
+ *  **DEPRECATED:** Writes all files in the archive to the given path
+>>>>>>> Updated documentation
+>>>>>>> Updated documentation
  *
  *  @param filePath  The destination path of the unarchived files
  *  @param overwrite YES to overwrite files in the destination directory, NO otherwise
@@ -448,9 +462,16 @@ extern NSString *URKErrorDomain;
                  error:(NSError **)error;
 =======
               progress:(nullable void (^)(URKFileInfo *currentFile, CGFloat percentArchiveDecompressed))progressBlock
+<<<<<<< HEAD
                  error:(NSError **)error __deprecated_msg("Use extractFilesTo:overwrite:error: instead, and use NSProgress as described in the README");
 >>>>>>> Deprecated methods that take a progress block argument, and created versions without them
+<<<<<<< HEAD
 >>>>>>> Deprecated methods that take a progress block argument, and created versions without them
+=======
+=======
+                 error:(NSError **)error __deprecated_msg("Use extractFilesTo:overwrite:error: instead, and if using the progress block, replace with NSProgress as described in the README");
+>>>>>>> Updated documentation
+>>>>>>> Updated documentation
 
 /**
  *  **DEPRECATED:** Unarchive a single file from the archive into memory
@@ -468,8 +489,7 @@ extern NSString *URKErrorDomain;
                            error:(NSError **)error;
 
 /**
- *  **DEPRECATED:** Unarchive a single file from the archive into memory. Supports NSProgress for progress reporting, which also
- *  allows cancellation in the middle of extraction
+ *  **DEPRECATED:** Unarchive a single file from the archive into memory
  *
 >>>>>>> Deprecated methods that take a progress block argument, and created versions without them
  *  @param fileInfo      The info of the file within the archive to be expanded. Only the filename property is used
@@ -491,9 +511,16 @@ extern NSString *URKErrorDomain;
                            error:(NSError **)error;
 =======
                         progress:(nullable void (^)(CGFloat percentDecompressed))progressBlock
+<<<<<<< HEAD
                            error:(NSError **)error __deprecated_msg("Use extractData:error: instead, and use NSProgress as described in the README");
 >>>>>>> Deprecated methods that take a progress block argument, and created versions without them
+<<<<<<< HEAD
 >>>>>>> Deprecated methods that take a progress block argument, and created versions without them
+=======
+=======
+                           error:(NSError **)error __deprecated_msg("Use extractData:error: instead, and if using the progress block, replace with NSProgress as described in the README");
+>>>>>>> Updated documentation
+>>>>>>> Updated documentation
 
 /**
  *  Unarchive a single file from the archive into memory. Supports NSProgress for progress reporting, which also
@@ -531,9 +558,13 @@ extern NSString *URKErrorDomain;
                                    error:(NSError **)error;
 
 /**
+<<<<<<< HEAD
  *  **DEPRECATED:** Unarchive a single file from the archive into memory. Supports NSProgress for progress reporting, which also
  *  allows cancellation in the middle of extraction
 >>>>>>> Deprecated methods that take a progress block argument, and created versions without them
+=======
+ *  **DEPRECATED:** Unarchive a single file from the archive into memory
+>>>>>>> Updated documentation
  *
  *  @param filePath      The path of the file within the archive to be expanded
  *  @param progressBlock Called every so often to report the progress of the extraction
@@ -547,6 +578,7 @@ extern NSString *URKErrorDomain;
 - (nullable NSData *)extractDataFromFile:(NSString *)filePath
                                 progress:(nullable void (^)(CGFloat percentDecompressed))progressBlock
 <<<<<<< HEAD
+<<<<<<< HEAD
                                    error:(NSError **)error __deprecated_msg("Use extractDataFromFile:error: instead, and if using the progress block, replace with NSProgress as described in the README");
 
 /**
@@ -554,6 +586,9 @@ extern NSString *URKErrorDomain;
 >>>>>>> Added NSProgress+cancellation support to a couple more methods for which it makes sense, and cleaned up some code along the way
 =======
                                    error:(NSError **)error __deprecated_msg("Use extractDataFromFile:error: instead, and use NSProgress as described in the README");
+=======
+                                   error:(NSError **)error __deprecated_msg("Use extractDataFromFile:error: instead, and if using the progress block, replace with NSProgress as described in the README");
+>>>>>>> Updated documentation
 
 /**
 >>>>>>> Deprecated methods that take a progress block argument, and created versions without them
