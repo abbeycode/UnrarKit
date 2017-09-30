f2130de215f8774ef021ef9531ca5702bb19b0f5
Dov Frankel <dov@abbey-code.com>
2017-09-29 22:23:27 -0400

    Reverted change to Carthage validation script

--- Scripts/carthage-validate.sh
+++ Scripts/carthage-validate.sh
@@ -29,7 +29,7 @@ rm -rf Carthage
 
 echo "$REPO \"$COMMIT\"" > Cartfile
 
-carthage build --no-skip-current
+carthage bootstrap --configuration Debug --verbose
 EXIT_CODE=$?
 
 echo "Checking for build products..."
