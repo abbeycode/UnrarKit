10fc81fc50adff2e5b65747adc14ae895efed255
Dov Frankel <dov@abbey-code.com>
2017-10-04 08:21:47 -0400

    Using macro for rar.hpp import

--- Classes/Categories/NSString+UnrarKit.mm
+++ Classes/Categories/NSString+UnrarKit.mm
@@ -5,15 +5,9 @@
 //
 
 #import "NSString+UnrarKit.h"
+#import "UnrarKitMacros.h"
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wcast-align"
-#pragma clang diagnostic ignored "-Wextra-semi"
-#pragma clang diagnostic ignored "-Wold-style-cast"
-#pragma clang diagnostic ignored "-Wpadded"
-#pragma clang diagnostic ignored "-Wreserved-id-macro"
-#pragma clang diagnostic ignored "-Wshorten-64-to-32"
-#pragma clang diagnostic ignored "-Wundef"
+RarHppIgnore
 #import "rar.hpp"
 #pragma clang diagnostic pop
 
