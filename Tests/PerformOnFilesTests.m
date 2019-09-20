b171b7152b4e39c65189fb25485e0c9c0e735b39
Dov Frankel <dov@abbey-code.com>
2019-09-19 23:38:53 -0400

    Moved PerformOnFilesTests into their own file, and added new test classes into Xcode project

---
+++ Tests/PerformOnFilesTests.m
@@ -0,0 +1,104 @@
+//
+//  PerformOnFilesTests.m
+//  UnrarKit
+//
+//
+
+#import "URKArchiveTestCase.h"
+
+@interface PerformOnFilesTests : URKArchiveTestCase
+
+@end
+
+@implementation PerformOnFilesTests
+
+- (void)testPerformOnFiles
+{
+    NSArray *testArchives = @[@"Test Archive.rar",
+                              @"Test Archive (Password).rar",
+                              @"Test Archive (Header Password).rar"];
+    
+    NSSet *expectedFileSet = [self.testFileURLs keysOfEntriesPassingTest:^BOOL(NSString *key, id obj, BOOL *stop) {
+        return ![key hasSuffix:@"rar"] && ![key hasSuffix:@"md"];
+    }];
+    
+    NSArray *expectedFiles = [[expectedFileSet allObjects] sortedArrayUsingSelector:@selector(compare:)];
+    
+    for (NSString *testArchiveName in testArchives) {
+        NSURL *testArchiveURL = self.testFileURLs[testArchiveName];
+        NSString *password = ([testArchiveName rangeOfString:@"Password"].location != NSNotFound
+                              ? @"password"
+                              : nil);
+        URKArchive *archive = [[URKArchive alloc] initWithURL:testArchiveURL password:password error:nil];
+        
+        __block NSUInteger fileIndex = 0;
+        NSError *error = nil;
+        
+        [archive performOnFilesInArchive:
+         ^(URKFileInfo *fileInfo, BOOL *stop) {
+             NSString *expectedFilename = expectedFiles[fileIndex++];
+             XCTAssertEqualObjects(fileInfo.filename, expectedFilename, @"Unexpected filename encountered");
+         } error:&error];
+        
+        XCTAssertNil(error, @"Error iterating through files");
+        XCTAssertEqual(fileIndex, expectedFiles.count, @"Incorrect number of files encountered");
+    }
+}
+
+- (void)testPerformOnFiles_Unicode
+{
+    NSSet *expectedFileSet = [self.unicodeFileURLs keysOfEntriesPassingTest:^BOOL(NSString *key, id obj, BOOL *stop) {
+        return ![key hasSuffix:@"rar"] && ![key hasSuffix:@"md"];
+    }];
+    
+    NSArray *expectedFiles = [[expectedFileSet allObjects] sortedArrayUsingSelector:@selector(compare:)];
+    
+    NSURL *testArchiveURL = self.unicodeFileURLs[@"Ⓣest Ⓐrchive.rar"];
+    URKArchive *archive = [[URKArchive alloc] initWithURL:testArchiveURL error:nil];
+    
+    __block NSUInteger fileIndex = 0;
+    NSError *error = nil;
+    
+    [archive performOnFilesInArchive:
+     ^(URKFileInfo *fileInfo, BOOL *stop) {
+         NSString *expectedFilename = expectedFiles[fileIndex++];
+         XCTAssertEqualObjects(fileInfo.filename, expectedFilename, @"Unexpected filename encountered");
+     } error:&error];
+    
+    XCTAssertNil(error, @"Error iterating through files");
+    XCTAssertEqual(fileIndex, expectedFiles.count, @"Incorrect number of files encountered");
+}
+
+#if !TARGET_OS_IPHONE
+- (void)testPerformOnFiles_Ordering
+{
+    NSArray *testFilenames = @[@"AAA.txt",
+                               @"BBB.txt",
+                               @"CCC.txt"];
+    
+    NSFileManager *fm = [NSFileManager defaultManager];
+    
+    NSMutableArray *testFileURLs = [NSMutableArray array];
+    
+    // Touch test files
+    [testFilenames enumerateObjectsUsingBlock:^(NSString *filename, NSUInteger idx, BOOL *stop) {
+        NSURL *outputURL = [self.tempDirectory URLByAppendingPathComponent:filename];
+        XCTAssertTrue([fm createFileAtPath:outputURL.path contents:nil attributes:nil], @"Failed to create test file: %@", filename);
+        [testFileURLs addObject:outputURL];
+    }];
+    
+    // Create RAR archive with test files, reversed
+    NSURL *reversedArchiveURL = [self archiveWithFiles:testFileURLs.reverseObjectEnumerator.allObjects];
+    
+    NSError *error = nil;
+    __block NSUInteger index = 0;
+    
+    URKArchive *archive = [[URKArchive alloc] initWithURL:reversedArchiveURL error:nil];
+    [archive performOnFilesInArchive:^(URKFileInfo *fileInfo, BOOL *stop) {
+        NSString *expectedFilename = testFilenames[index++];
+        XCTAssertEqualObjects(fileInfo.filename, expectedFilename, @"Archive files not iterated through in correct order");
+    } error:&error];
+}
+#endif
+
+@end
