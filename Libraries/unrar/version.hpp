dcd51ceb5ec032bc2d5837419cf588aba09777e1
Dov Frankel <dov@abbey-code.com>
2020-10-01 11:04:41 -0400

    Upgraded unrar library to v5.9.4

--- Libraries/unrar/version.hpp
+++ Libraries/unrar/version.hpp
@@ -1,6 +1,6 @@
 #define RARVER_MAJOR     5
-#define RARVER_MINOR    80
-#define RARVER_BETA      1
-#define RARVER_DAY      30
-#define RARVER_MONTH     8
-#define RARVER_YEAR   2019
+#define RARVER_MINOR    91
+#define RARVER_BETA      0
+#define RARVER_DAY      25
+#define RARVER_MONTH     6
+#define RARVER_YEAR   2020
