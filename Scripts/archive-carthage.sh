90030c32c40af586ac3ff9394df8fbb679311757
Dov Frankel <dov@abbey-code.com>
2020-10-02 13:14:26 -0400

    Also updated the archive-carthage.sh script so it doesn't fail either

--- Scripts/archive-carthage.sh
+++ Scripts/archive-carthage.sh
@@ -4,7 +4,9 @@ set -ev
 
 # Archives the Carthage packages, and prints the name of the archive
 
-carthage build --no-skip-current
-carthage archive
+# Employing a workaround until Xcode 12 builds are fixed
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+source "${SCRIPTPATH}"/carthage.sh build --no-skip-current
+source "${SCRIPTPATH}"/carthage.sh archive
 
 export ARCHIVE_PATH="UnrarKit.framework.zip"
